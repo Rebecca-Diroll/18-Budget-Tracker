a25b7eb530cad524e4848e156adac8dbd0969cc9
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-04 21:52:34 -0400

    Update service-worker.js files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,10 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/index.js',
     '/manifest.webmanifest',
-    '/styles.css'
+    '/styles.css',
+    "icons/icon-192x192.png",
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
 // install
